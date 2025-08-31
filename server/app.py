0617663bfdd38300fc67de83cfc73d5f1f7379df
Anastasia Wakoli <anastasiashikhaya@gmail.com>
2025-09-01 00:52:29 +0300

    anna mnoma

--- server/app.py
+++ server/app.py
@@ -47,4 +47,4 @@ if __name__ == '__main__':
     with app.app_context():
         # Create all tables
         db.create_all()
-    app.run(port=5555, debug=True)
+    app.run(port=5000, debug=True)
